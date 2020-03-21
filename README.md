930ee9f727a1668f2f43d1764b8e965d687ed1ff
NicolasGrosjean <grosjen@hotmail.fr>
2020-03-21 10:09:45 +0100

    Add script to apply diff
    Add a script to apply diff from source files for EUIV and sooner

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ activate paradox_loc
 ```
 
 ## Usage
+
+### Add missing lines for Imperator Rome and sooner
 ```
 python src/add_missing_lines.py <source_dir> <dest_dir>
 ```
@@ -27,6 +29,25 @@ Example to add in French files missing lines from English files
 ```
 python src/add_missing_lines.py "<...>\game\localization\english" "<...>\game\localization\french"
 ```
+
+### Apply diff for EUIV and sooner
+```
+python src/apply_diff.py <old_source_file> <new_source_file> <dest_file>
+```
+
+The destination file is override so don't hesitate to have a backup before running this.
+The new and edited lines contains `:9 "`
+
+Optionnal parameters :
+* ```-space_prefix <value>``` : Prefix (generally spaces) before key in files
+* ```-keep_edited``` : Keep destination value in case of edition.
+The default behavior replace the destination value by the new source value.
+
+Example to apply the English update from 0.11.1 to 0.11.2 to Russia file in French:
+```
+<...>\0.11.1\localisation\KR_Russia_l_english.yml <...>\0.11.2\localisation\KR_Russia_l_english.yml <...>\Translation_FR\localisation\KR_Russia_l_french.yml -space_prefix "  " -keep_edited
+```
+After running this, the new and edited lines need to be fixed by hand by seeking `:9 "`
 
 ## Tests
 
---
+++ tests/test_apply_diff.py
@@ -0,0 +1,105 @@
+import unittest
+
+import os
+import shutil
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+from src.apply_diff import apply_diff
+
+
+class TestApplyDiff(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.data_dir = os.path.join('data', 'apply_diff')
+        for file in os.listdir(os.path.join(cls.data_dir, 'dest')):
+            shutil.copyfile(os.path.join(cls.data_dir, 'dest', file), os.path.join(cls.data_dir, file))
+
+    def test_apply_diff_same_sources(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_0.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_0.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_0.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  KEY0:0 "valeur0"')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_same_sources_unordered_dest(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_0.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_0.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_1.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  KEY0:0 "valeur0"')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_same_sources_with_comments(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_2.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_2.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_2.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  # Ideas')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY0:0 "valeur0"')
+        self.assertEqual(lines[3].replace('\n', ''), '  ')
+        self.assertEqual(lines[4].replace('\n', ''), '  # Events')
+        self.assertEqual(lines[5].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_new_line(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_3.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_2.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_3.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  # Ideas')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY0:9 "value0"')
+        self.assertEqual(lines[3].replace('\n', ''), '  ')
+        self.assertEqual(lines[4].replace('\n', ''), '  # Events')
+        self.assertEqual(lines[5].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_delete_line(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_2.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_4.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_4.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  # Events')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_edited_line(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_2.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_5.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_5.yml'))
+        apply_diff(old_source, new_source, dest)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  # Ideas')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY0:9 "value42"')
+        self.assertEqual(lines[3].replace('\n', ''), '  ')
+        self.assertEqual(lines[4].replace('\n', ''), '  # Events')
+        self.assertEqual(lines[5].replace('\n', ''), '  KEY1:2 "valeur1"')
+
+    def test_apply_diff_edited_line_keep_edited(self):
+        old_source = os.path.abspath(os.path.join('data', 'apply_diff', 'old_source_2.yml'))
+        new_source = os.path.abspath(os.path.join('data', 'apply_diff', 'new_source_5.yml'))
+        dest = os.path.abspath(os.path.join('data', 'apply_diff', 'dest_6.yml'))
+        apply_diff(old_source, new_source, dest, keep_edited=True)
+        with open(dest, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+        self.assertEqual(lines[0].replace('\n', ''), 'l_french:')
+        self.assertEqual(lines[1].replace('\n', ''), '  # Ideas')
+        self.assertEqual(lines[2].replace('\n', ''), '  KEY0:9 "valeur0"')
+        self.assertEqual(lines[3].replace('\n', ''), '  ')
+        self.assertEqual(lines[4].replace('\n', ''), '  # Events')
+        self.assertEqual(lines[5].replace('\n', ''), '  KEY1:2 "valeur1"')
