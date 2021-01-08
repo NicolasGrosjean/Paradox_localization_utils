783cd49d49a324988d2c8f43a3bf8aa2a24cba23
NicolasGrosjean <grosjen@hotmail.fr>
2021-01-08 22:43:04 +0100

    Fix add_missing_lines for IR
    - Remove old_lines file
    - Add new required arguments

--- src/add_missing_lines.py
+++ src/add_missing_lines.py
@@ -15,7 +15,7 @@ def get_args():
     return parser.parse_args()
 
 
-def add_missing_lines_one_file(source_file_path, dest_file_path, old_lines_file_path, dest_texts, dest_first_line):
+def add_missing_lines_one_file(source_file_path, dest_file_path, dest_texts, dest_first_line):
     with open(source_file_path, 'r', encoding='utf8') as f:
         source_lines = f.readlines()
 
@@ -36,15 +36,9 @@ def add_missing_lines_one_file(source_file_path, dest_file_path, old_lines_file_
                     continue
                 if key in dest_texts and (dest_texts[key] != '' or value == ''):
                     f.write(dest_texts[key])
-                    if old_lines_file_path is not None:
-                        del dest_texts[key]
                 else:
                     dest_version = '' if version is None else str(version)
                     f.write(' ' + key + ':' + str(dest_version) + ' "' + value + '"\n')
-    if old_lines_file_path is not None:
-        with open(old_lines_file_path, 'a', encoding='utf8') as f:
-            for dest_line in dest_texts.values():
-                f.write(dest_line)
 
 
 def add_missing_lines_imperator(source_dir, dest_dir):
@@ -55,10 +49,13 @@ def add_missing_lines_imperator(source_dir, dest_dir):
     :return:
     """
     rel_to_dest_abs_path = dict()
+    dest_texts = dict()
     for root, _, files in os.walk(dest_dir):
         for file in files:
             abs_path = os.path.abspath(os.path.join(root, file))
             rel_to_dest_abs_path[abs_path[:abs_path.find('_l_')].replace(dest_dir, '')] = abs_path
+            file_dest_texts, _ = file_to_keys_and_lines(abs_path)
+            dest_texts = {**dest_texts, **file_dest_texts}
     for root, _, files in os.walk(source_dir):
         for file in files:
             abs_path = os.path.abspath(os.path.join(root, file))
@@ -66,8 +63,9 @@ def add_missing_lines_imperator(source_dir, dest_dir):
             if rel_path not in rel_to_dest_abs_path:
                 print(f'File {rel_path} doesn\'t exists for destination language')
             else:
-                add_missing_lines_one_file(abs_path, rel_to_dest_abs_path[rel_path],
-                                          os.path.abspath(os.path.join(dest_dir, '..', 'old_lines.yml')))
+                dest_lang = args.dest_dir.split('\\')[-1]
+                add_missing_lines_one_file(abs_path, rel_to_dest_abs_path[rel_path], dest_texts,
+                                           '\ufeffl_' + dest_lang + ':\n')
 
 
 def add_missing_lines_eu_hoi_stellaris(source_dir, source_lang, dest_lang):
@@ -97,12 +95,12 @@ def add_missing_lines_eu_hoi_stellaris(source_dir, source_lang, dest_lang):
                     dest_file_path = rel_to_dest_abs_path[rel_path]
                 else:
                     dest_file_path = abs_path.replace(source_lang + '.yml', dest_lang + '.yml')
-                add_missing_lines_one_file(abs_path, dest_file_path, None, dest_texts, '\ufeffl_' + dest_lang + ':\n')
+                add_missing_lines_one_file(abs_path, dest_file_path, dest_texts, '\ufeffl_' + dest_lang + ':\n')
 
 
 if __name__ == '__main__':
     args = get_args()
-    if (args.source_lang is None) and (args.dir_lang is None):
+    if (args.source_lang is None) and (args.dest_lang is None):
         add_missing_lines_imperator(args.source_dir, args.dest_dir)
     else:
         add_missing_lines_eu_hoi_stellaris(args.source_dir, args.source_lang, args.dest_lang)
